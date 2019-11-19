6dfc3292b7e1a53db9a3858524d414a5b866cf0b
Nguyễn Văn Đạt <datnv>
2019-11-19 16:31:50 +0700

    login + register

---
+++ client/src/components/Login.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import {Link} from 'react-router-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    console.log(this.props)
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form" style={{margin : "auto"}}>
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <Link className="login-form-forgot" to="">
+            Forgot password
+          </Link>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <Link to="/register">register now!</Link>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+export const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
