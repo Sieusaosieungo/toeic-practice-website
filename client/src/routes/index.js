0f87e9785060a0c273410443762c039eedd73648
Nguyễn Văn Đạt <datnv>
2019-11-19 16:31:50 +0700

    login + register

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import {WrappedNormalLoginForm} from '../components/Login'
+import {WrappedRegistrationForm} from '../components/Register'
 const routes = [];
 
 export default () => (
   <Switch>
+    <Route path="/login" exact={true} component={WrappedNormalLoginForm} />
+    <Route path="/register" exact={true} component={WrappedRegistrationForm} />
     <Route path="/" component={() => <div>hello</div>} />
   </Switch>
 );
