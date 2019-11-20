263774864aaf901979a3110afcad22e8006862d7
datnv <dat.nv160952@sis.hust.edu.vn>
2019-11-20 22:16:12 +0700

    Direction exam

---
+++ client/src/pages/Exam/Intro/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+import './style.scss'
+const prefixCls = 'home';
+
+const Intro = ({}) => {
+  return <div className={`${prefixCls}`}> 
+    <div className="" style={{marginTop : "1.5em"}}>
+      <Row>
+        Ngạc nhiên chưa
+      </Row>
+      
+    </div>
+  </div>;
+};
+
+export default Intro;
