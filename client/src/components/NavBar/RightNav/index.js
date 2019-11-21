3cedd4183433cdc045bbafa91d6d0dd94a021cde
datnv <dat.nv160952@sis.hust.edu.vn>
2019-11-21 22:36:26 +0700

    add image

--- client/src/components/NavBar/RightNav/index.js
+++ client/src/components/NavBar/RightNav/index.js
@@ -107,7 +107,7 @@ const RightMenu = ({ mode, user, accessTokenStore, dispatch}) => {
           <Avatar
             src={
               user.images ||
-              'https://cdn.eva.vn/upload/4-2019/images/2019-11-06/sinh-ra-trong-gia-dinh-viet-nhung-co-be-nay-lai-mang-ve-dep-tay-la-ky-untitled-19-1573053449-116-width600height750.jpg'
+              "https://toeic-practice.herokuapp.com" + user.data.results.user.avatar
             }
             style={{marginRight : "10px"}}
           />
