12f9d0b4c6301c73013f9d291d64675e5ae8a9d2
viethungbk <anhhung021098@gmail.com>
2019-11-28 12:53:32 +0700

    Add add question api

---
+++ src/routes/question.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const asyncWrap = require('../middlewares/asyncWrap');
+const {
+  addBasicQuestionInfo,
+  addSubQuestions,
+} = require('../controllers/question.controller');
+
+// Add basic question's info: image, audio, scripts, level, paragraph
+router.post('/basic-info', asyncWrap(addBasicQuestionInfo));
+// Add subQuestion after added basic question
+router.post('/sub-questions', asyncWrap(addSubQuestions));
+
+module.exports = router;
