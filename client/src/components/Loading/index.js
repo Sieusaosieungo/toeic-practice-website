b874526869b2a6ba691e359048bf1e42e0f623a3
NightWolf1x <trantrunghuynh1998@gmail.com>
2019-11-30 00:28:23 +0700

    fix render page admin

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AtomSpinner } from 'react-epic-spinners';
+
 import './style.scss';
 
+const prefixCls = 'loading';
+
 const Loading = ({}) => {
-  return <div></div>;
+  return (
+    <div className={`${prefixCls}`}>
+      <AtomSpinner color="#1890FF" />
+    </div>
+  );
 };
 
 export default Loading;
