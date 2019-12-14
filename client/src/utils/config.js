d1622ed7eb26960bf6347bcbc35d427abb3c6888
NightWolf1x <trantrunghuynh1998@gmail.com>
2019-12-14 15:47:19 +0700

    Fixed part 5

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,8 @@
 const config = {
-  API_URL: 'http://localhost:5000',
-  IMG_URL: 'http://localhost:5000',
+  API_URL: 'http://202.191.56.159:2510',
+  IMG_URL: 'http://202.191.56.159:2510',
 };
+// http://202.191.56.159:2510
+// http://localhost:5000
 
 export default config;
